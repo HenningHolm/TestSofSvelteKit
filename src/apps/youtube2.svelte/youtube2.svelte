68806e8850fdf667f14e458b5a650e96671ef539
henningholm <henningholm90@gmail.com>
2023-03-31 13:56:38 +0200

    Test karaoke

--- src/apps/youtube2.svelte/youtube2.svelte
+++ src/apps/youtube2.svelte/youtube2.svelte
@@ -18,8 +18,10 @@
         <SystemWindow on:close>
             <div id="htmlContent" slot="canvas">
                 <div bind:this={maine} id="main">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/hLQl3WQQoQ0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
+                    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/hLQl3WQQoQ0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/sPMA1tqWuf4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            
+                </div>
     
             </div>
             <!-- <ClockSettings
